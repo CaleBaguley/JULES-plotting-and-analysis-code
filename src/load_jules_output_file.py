a79a56c8f2bb92418e9e64f8f1e8841fd075c37b
CaleBaguley <jb14389@bristol.ac.uk>
2024-03-14 10:40:45 +0000

    Created code to load in netcdf file via xarray. Can be loaded into pandas data frame.

---
+++ src/load_jules_output_file.py
@@ -0,0 +1,37 @@
+"""
+This file contains functions to load a JULES output file and convert it into a pandas dataframe.
+"""
+
+from xarray import open_dataset
+
+
+def load_jules_output_file_pandas(file_path):
+    """
+    Load a JULES output file and convert it into a pandas dataframe.
+
+    Args:
+    file_path (str): The file path to the JULES output file.
+
+    Returns:
+    df (pd.DataFrame): The JULES output file as a pandas dataframe.
+    """
+    # Load the JULES output file
+    file = open_dataset(file_path)
+
+    return file.to_dataframe()
+
+
+def load_jules_output_file_xarray(file_path):
+    """
+    Load a JULES output file and convert it into an xarray dataset.
+
+    Args:
+    file_path (str): The file path to the JULES output file.
+
+    Returns:
+    file (xarray.Dataset): The JULES output file as an xarray dataset.
+    """
+    # Load the JULES output file
+    file = open_dataset(file_path)
+
+    return file
