bc1cc14d4cbc2e44d59d311b81a24c215c9583d9
CaleBaguley <jb14389@bristol.ac.uk>
2024-03-18 09:47:01 +0000

    Changed file name. plot_daily_gpp.py -> plot_daily.py. Changed function name from plot_daily_gpp -> plot_daily

--- src/plotting/plot_daily.py
+++ src/plotting/plot_daily.py
@@ -1,20 +1,21 @@
 """
-Plot daily GPP for all sites.
+Plot daily value.
 """
 
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-def plot_daily_gpp(data_xarray, gpp_key, smoothing = 1, x_range = None, c ='blue'):
+def plot_daily(data_xarray, col_key, smoothing = 1, x_range = None, c ='blue', label = None):
     """
     Plot the daily GPP for a site.
 
     Args:
     data_xarray (xarray.Dataset): The input xarray dataset.
-    gpp_key (str): The key for the GPP variable.
+    col_key (str): The key for the GPP variable.
     smoothing (int): The number of days to smooth the data by.
     x_range (list): The range of dates to plot, in the form [date].
     c (str): The color to plot the data.
+    label (str): The label for the date in the plot's legend.
 
     Returns:
     None
@@ -24,12 +25,12 @@ def plot_daily_gpp(data_xarray, gpp_key, smoothing = 1, x_range = None, c ='blue
     data_xarray_daily_total = data_xarray.resample(time="1D").sum()
 
     # Calculate median and 95% confidence intervals from the daily total GPP
-    data_xarray_daily_total['median'] = data_xarray_daily_total[gpp_key].rolling(time=smoothing, center = True).construct('tmp').quantile(.50, dim='tmp')
-    data_xarray_daily_total['lower'] = data_xarray_daily_total[gpp_key].rolling(time=smoothing, center = True).construct('tmp').quantile(.96, dim='tmp')
-    data_xarray_daily_total['upper'] = data_xarray_daily_total[gpp_key].rolling(time=smoothing, center = True).construct('tmp').quantile(.04, dim='tmp')
+    data_xarray_daily_total['median'] = data_xarray_daily_total[col_key].rolling(time=smoothing, center = True).construct('tmp').quantile(.50, dim='tmp')
+    data_xarray_daily_total['lower'] = data_xarray_daily_total[col_key].rolling(time=smoothing, center = True).construct('tmp').quantile(.96, dim='tmp')
+    data_xarray_daily_total['upper'] = data_xarray_daily_total[col_key].rolling(time=smoothing, center = True).construct('tmp').quantile(.04, dim='tmp')
 
     # Plot the daily GPP for all sites
-    data_xarray_daily_total['median'].plot(color = c)
+    data_xarray_daily_total['median'].plot(color = c, label=label)
     plt.fill_between(data_xarray_daily_total['time'].values,
                      data_xarray_daily_total['lower'].values[:, 0, 0],
                      data_xarray_daily_total['upper'].values[:, 0, 0],
@@ -43,6 +44,6 @@ def plot_daily_gpp(data_xarray, gpp_key, smoothing = 1, x_range = None, c ='blue
         plt.xlim(min_x, max_x)
 
     plt.xlabel('Date')
-    plt.ylabel('GPP (gC m-2 day-1)')
+    plt.ylabel(col_key)
 
     return None
