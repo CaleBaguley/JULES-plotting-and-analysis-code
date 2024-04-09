b8dbcc40f6abfff7cd933d248303274c5fddeee0
CaleBaguley <jb14389@bristol.ac.uk>
2024-04-09 11:11:16 +0100

    Added inputs for observational data.

--- src/plotting/plot_flux_results.py
+++ src/plotting/plot_flux_results.py
@@ -8,13 +8,16 @@ from src.plotting.plot_daily import plot_daily_total, plot_daily_mean
 from src.plotting.plot_col_at_daily_time import plot_col_at_daily_time
 from src.load_jules_output_file import open_dataset
 
-def plot_flux_data(data_xarrays, labels, colours, title = None, timestep_s = 3600.0):
+def plot_flux_data(data_xarrays, observation_xarrays, labels, data_colours, observation_colours,
+                   title = None, timestep_s = 3600.0, smoothing = None, x_range = None):
 
     """
     Plot the flux data from a set of jules outputs.
     :param data_xarrays: The input xarray datasets. Xarray.Dataset or list of Xarray.Dataset
+    :param observation_xarrays: The observational data. Xarray.Dataset
     :param labels: The labels to plot the data with. String or list of strings.
-    :param colours: The colours to plot the data in. String or list of strings.
+    :param data_colours: The colours to plot the data in. String or list of strings.
+    :param observation_colours: The colours to plot the observational data in. String.
     :param title: The title of the plot. String.
     :param timestep_s: The time step of the input data in seconds. Float.
     :return: None
@@ -32,10 +35,10 @@ def plot_flux_data(data_xarrays, labels, colours, title = None, timestep_s = 360
     elif(type(labels) != list):
         raise ValueError("The input labels must be a list of strings.")
 
-    if(type(colours) == str):
-        colours = [colours]
-    elif(type(colours) != list):
-        raise ValueError("The input colours must be a list of strings.")
+    if(type(data_colours) == str):
+        data_colours = [data_colours]
+    elif(type(data_colours) != list):
+        raise ValueError("The input data_colours must be a string or list of strings.")
 
 # --- Data processing. ---
     # Convert GPP data units from kgC m-2 s-1 to gC m-2 timestep-1
@@ -58,24 +61,29 @@ def plot_flux_data(data_xarrays, labels, colours, title = None, timestep_s = 360
 
     # ---- Plot the GPP data ----
     for i in range(len(data_xarrays)):
-        plot_daily_total(data_xarrays[i], "gpp_gb", c = colours[i], label = labels[i], axs = axs[0], title = "")
+        plot_daily_total(data_xarrays[i], "gpp_gb", c = data_colours[i], label = labels[i], axs = axs[0],
+                         title = "", smoothing = smoothing, x_range = x_range)
 
     # set the y-axis label
     axs[0].set_ylabel("GPP (gC m-2 day-1)")
 
     # ---- Plot the latent heat data ----
     for i in range(len(data_xarrays)):
-        plot_daily_mean(data_xarrays[i], "latent_heat", c = colours[i], label = labels[i], axs = axs[1], title = "")
+        plot_daily_mean(data_xarrays[i], "latent_heat", c = data_colours[i], label = labels[i], axs = axs[1],
+                        title = "", smoothing = smoothing, x_range = x_range)
 
     # set the y-axis label
     axs[1].set_ylabel("Latent Heat (W m-2)")
 
+
+    #TODO: Rerun profit max and SOX code with psi_leaf_pft variable as output.
     # ---- Plot the leaf water potential data ----
     for i in range(len(data_xarrays)):
         data_xarrays[i]["psi_leaf_mean"] = data_xarrays[i]["psi_leaf_pft"].mean(dim= "pft")
         print(data_xarrays[i]["psi_leaf_pft"])
         plot_col_at_daily_time(data_xarrays[i], "psi_leaf_mean", "12:00:00",
-                               c = colours[i], label = labels[i], title = "", axis = axs[2])
+                               c = data_colours[i], label = labels[i], title = "", axis = axs[2], smoothing = smoothing)
+
 
     # set the y-axis label
     axs[2].set_ylabel("Leaf Water Potential (MPa)")
@@ -85,9 +93,17 @@ def plot_flux_data(data_xarrays, labels, colours, title = None, timestep_s = 360
 
 if __name__ == "__main__":
     # Load the JULES output files
-    file_path_1 = "../../../data/data_runs/stomatal_optimisation_runs/plumber2_runs/Prof_Max_JULES_runs/AT_Neu-JULES_vn7.4-presc0.D.2006.nc"
-    file_1 = open_dataset(file_path_1)
+    data_file_paths = ["../../../data/data_runs/stomatal_optimisation_runs/plumber2_runs/JULES_PMax_run/AT_Neu-JULES_vn7.4-presc0.Stom_opt.nc"]
+
+    data_files = []
+    for path in data_file_paths:
+        data_files.append(open_dataset(path))
+
+    # Load the observational data
+    observation_file_path = "../../../data/PLUMBER2/met/AT-Neu_2002-2012_FLUXNET2015_Met.nc"
+
+    observation_file = open_dataset(observation_file_path)
 
     # Plot the flux data
-    plot_flux_data(file_1, "JULES Output", "blue", "JULES Output")
+    plot_flux_data(data_files[0], observation_file, "JULES Output", "blue", "orange", "JULES Output", smoothing = 30)
     plt.show()
