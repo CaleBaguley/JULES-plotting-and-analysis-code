21182cd5523b5768faed363419b669e289cf32bf
CaleBaguley <jb14389@bristol.ac.uk>
2024-04-09 12:17:11 +0100

    Removed hard coded timestep conversion for GPP.

--- src/plotting/plot_flux_results.py
+++ src/plotting/plot_flux_results.py
@@ -9,7 +9,7 @@ from src.plotting.plot_col_at_daily_time import plot_col_at_daily_time
 from src.load_jules_output_file import open_dataset
 
 def plot_flux_data(data_xarrays, observation_xarray, labels, data_colours, observation_colours,
-                   title = None, timestep_s = 3600.0, smoothing = None, x_range = None):
+                   title = None, smoothing = None, x_range = None):
 
     """
     Plot the flux data from a set of jules outputs.
@@ -19,7 +19,6 @@ def plot_flux_data(data_xarrays, observation_xarray, labels, data_colours, obser
     :param data_colours: The colours to plot the data in. String or list of strings.
     :param observation_colours: The colours to plot the observational data in. String.
     :param title: The title of the plot. String.
-    :param timestep_s: The time step of the input data in seconds. Float.
     :return: None
     """
 
@@ -40,20 +39,28 @@ def plot_flux_data(data_xarrays, observation_xarray, labels, data_colours, obser
     elif(type(data_colours) != list):
         raise ValueError("The input data_colours must be a string or list of strings.")
 
-# --- Data processing. ---
+    # --- Data processing. ---
+
+    # First we need to convert the units of the GPP data from both the JULES output files and the observation file.
+
     # Convert GPP data units from kgC m-2 s-1 to gC m-2 timestep-1
     # kgC -> gC: * 1000
-    # s-1 -> timestep-1: * timestep_s
+    # s-1 -> timestep-1: * timestep
     for i in range(len(data_xarrays)):
-        data_xarrays[i]["gpp_gb"] = data_xarrays[i]["gpp_gb"] * timestep_s * 1000
+        timestep = data_xarrays[i]["time"].values[1] - data_xarrays[i]["time"].values[0]
+        timestep = timestep.astype("timedelta64[s]").astype(int)
+        data_xarrays[i]["gpp_gb"] = data_files[i]["gpp_gb"] * 1000 * timestep
 
     # Convert GPP data units from umol m-2 s-1 to gC m-2 timestep-1
     # umol -> mol: * 1e-6
-    # mol -> g: * 12.01
-    # s-1 -> timestep-1: * timestep_s
-    observation_xarray["GPP"] = observation_xarray["GPP"] * timestep_s * 1e-6 * 12.01
+    # molCO2 -> molC: * 44/12.01
+    # molC -> gC: * 12.01
+    # s-1 -> timestep-1: * timestep
+    timestep = observation_xarray["time"].values[1] - observation_xarray["time"].values[0]
+    timestep = timestep.astype("timedelta64[s]").astype(int)
+    observation_xarray["GPP"] = observation_xarray["GPP"] * 1e-6 * 12.01 * timestep
 
-# --- Plot the data. ---
+    # --- Figure setup ---
     # Create figure with multiple subplots.
     fig, axs = plt.subplots(3, 1, figsize=(8, 5), sharex=True)
 
@@ -73,7 +80,7 @@ def plot_flux_data(data_xarrays, observation_xarray, labels, data_colours, obser
                          title = "", smoothing = smoothing, x_range = x_range)
 
     # Plot the observational data
-    plot_daily_total(observation_xarray, "GPP", c=observation_colours, label=labels[i], axs=axs[0],
+    plot_daily_total(observation_xarray, "GPP", c=observation_colours, label="Observation", axs=axs[0],
                      title="", smoothing=smoothing, x_range=x_range)
 
     # set the y-axis label
@@ -84,15 +91,20 @@ def plot_flux_data(data_xarrays, observation_xarray, labels, data_colours, obser
         plot_daily_mean(data_xarrays[i], "latent_heat", c = data_colours[i], label = labels[i], axs = axs[1],
                         title = "", smoothing = smoothing, x_range = x_range)
 
+    plot_daily_mean(observation_xarray, "Qle", c=observation_colours, label="Observation", axs=axs[1],
+                    title="", smoothing=smoothing, x_range=x_range)
+
     # set the y-axis label
     axs[1].set_ylabel("Latent Heat (W m-2)")
 
 
-    #TODO: Rerun profit max and SOX code with psi_leaf_pft variable as output.
     # ---- Plot the leaf water potential data ----
     for i in range(len(data_xarrays)):
+        data_xarrays[i]["psi_root_zone_mean"] = data_xarrays[i]["psi_root_zone_pft"].mean(dim= "pft")
         data_xarrays[i]["psi_leaf_mean"] = data_xarrays[i]["psi_leaf_pft"].mean(dim= "pft")
-        print(data_xarrays[i]["psi_leaf_pft"])
+
+        plot_col_at_daily_time(data_xarrays[i], "psi_root_zone_mean", "12:00:00",
+                               c=data_colours[i], label=labels[i], title="", axis=axs[2], smoothing=smoothing)
         plot_col_at_daily_time(data_xarrays[i], "psi_leaf_mean", "12:00:00",
                                c = data_colours[i], label = labels[i], title = "", axis = axs[2], smoothing = smoothing)
 
@@ -100,7 +112,7 @@ def plot_flux_data(data_xarrays, observation_xarray, labels, data_colours, obser
     # set the y-axis label
     axs[2].set_ylabel("Leaf Water Potential (MPa)")
 
-
+    axs[1].legend()
 
 
 if __name__ == "__main__":
@@ -122,7 +134,7 @@ if __name__ == "__main__":
     # Plot the flux data
     plot_flux_data(data_files[0],
                    observation_file,
-                   "JULES Output",
+                   "Profit Max",
                    "blue",
                    "orange",
                    "JULES Output",
